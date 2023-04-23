c3b5396de2bdc1eb183ba6387bdc903a2d6a27d3
Furkan Leylek <furkanyasinn.1@outlook.com>
2023-04-23 07:22:43 +0300

    DEPLOY

--- app/designs/page.jsx
+++ app/designs/page.jsx
@@ -1,20 +1,9 @@
 import React from 'react'
-import Slider from '@/components/slider'
 function Designs() {
 
-    const items = [
-        "SLİDER 1",
-        "SLİDER 2",
-        "SLİDER 3 ",
-        "SLİDER 4",
-        "SLİDER 5",
-        "SLİDER 6",
-        "SLİDER 7 ",
-        "SLİDER 8",
-    ]
     return (
         <div className='w-full h-full'>
-            <Slider items={items} />
+            DESİGNS
         </div>
     )
 }
